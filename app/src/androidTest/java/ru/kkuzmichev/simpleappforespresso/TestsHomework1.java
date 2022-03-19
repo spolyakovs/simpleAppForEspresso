be5f3f7e17f3b83eaa82ac7f44fa3e806a2ace54
Sergey Polyakov <sergpolyakow@mail.ru>
2022-03-19 21:28:00 +0300

    Added allure for all tests and attached allure-results

--- app/src/androidTest/java/ru/kkuzmichev/simpleappforespresso/TestsHomework1.java
+++ app/src/androidTest/java/ru/kkuzmichev/simpleappforespresso/TestsHomework1.java
@@ -1,26 +1,58 @@
 package ru.kkuzmichev.simpleappforespresso;
 
 import static androidx.test.espresso.Espresso.onView;
-import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static androidx.test.platform.app.InstrumentationRegistry.getInstrumentation;
+
+import android.Manifest;
+import android.os.Environment;
 
 import androidx.test.espresso.ViewInteraction;
-import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.rule.ActivityTestRule;
+import androidx.test.rule.GrantPermissionRule;
+import androidx.test.uiautomator.UiDevice;
 
 import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.TestWatcher;
+import org.junit.runner.Description;
 import org.junit.runner.RunWith;
 
-@RunWith(AndroidJUnit4.class)
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
+import io.qameta.allure.android.runners.AllureAndroidJUnit4;
+import io.qameta.allure.kotlin.Allure;
+
+@RunWith(AllureAndroidJUnit4.class)
 public class TestsHomework1 {
+
+    @Rule
+    public TestWatcher watcher = new TestWatcher() {
+        @Override
+        protected void failed(Throwable e, Description description) {
+            String className = description.getClassName();
+            className = className.substring(className.lastIndexOf('.') + 1);
+            String methodName = description.getMethodName();
+            takeScreenshot(className + "#" + methodName);
+        }
+    };
+
+    @Rule
+    public GrantPermissionRule mRuntimePermissionRule = GrantPermissionRule
+            .grant(
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                    Manifest.permission.READ_EXTERNAL_STORAGE
+            );
     @Rule
     public ActivityTestRule<MainActivity> activityTestRule = new ActivityTestRule<>(MainActivity.class);
 
     @Test
     public void positiveTest() {
+        Allure.feature("Positive test");
         ViewInteraction mainText = onView(
                 withId(R.id.text_home)
         );
@@ -29,11 +61,11 @@ public class TestsHomework1 {
                         withText("This is home fragment")
                 )
         );
-        mainText.perform(click());
     }
 
     @Test
     public void negativeTest() {
+        Allure.feature("Negative test");
         ViewInteraction mainText = onView(
                 withId(R.id.text_home)
         );
@@ -42,6 +74,22 @@ public class TestsHomework1 {
                         withText("Incorrect text")
                 )
         );
-        mainText.perform(click());
+    }
+
+    private void takeScreenshot(String name) {
+        File path = new File(Environment.getExternalStorageDirectory()
+                .getAbsolutePath() + "/screenshots/" );
+        if (!path.exists()) {
+            path.mkdirs();
+        }
+        UiDevice device = UiDevice.getInstance(getInstrumentation());
+        String filename = name + ".png";
+        device.takeScreenshot(new File(path, filename));
+        try {
+            Allure.attachment(filename, new FileInputStream(new File(path, filename)));
+        }
+        catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
     }
 }
--- app/src/androidTest/java/ru/kkuzmichev/simpleappforespresso/TestsHomework2.java
+++ app/src/androidTest/java/ru/kkuzmichev/simpleappforespresso/TestsHomework2.java
@@ -10,24 +10,38 @@ import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withContentDescription;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static androidx.test.platform.app.InstrumentationRegistry.getInstrumentation;
 
+import android.Manifest;
 import android.content.Intent;
+import android.os.Environment;
 
 import androidx.test.espresso.IdlingRegistry;
 import androidx.test.espresso.ViewInteraction;
 import androidx.test.espresso.intent.Intents;
-import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.rule.ActivityTestRule;
+import androidx.test.rule.GrantPermissionRule;
+import androidx.test.uiautomator.UiDevice;
 
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.TestWatcher;
+import org.junit.runner.Description;
 import org.junit.runner.RunWith;
 
 import static org.hamcrest.CoreMatchers.allOf;
 
-@RunWith(AndroidJUnit4.class)
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
+import io.qameta.allure.android.runners.AllureAndroidJUnit4;
+import io.qameta.allure.kotlin.Allure;
+import io.qameta.allure.kotlin.junit4.DisplayName;
+
+@RunWith(AllureAndroidJUnit4.class)
 public class TestsHomework2 {
 
     @Before
@@ -36,10 +50,29 @@ public class TestsHomework2 {
     }
 
     @Rule
+    public TestWatcher watcher = new TestWatcher() {
+        @Override
+        protected void failed(Throwable e, Description description) {
+            String className = description.getClassName();
+            className = className.substring(className.lastIndexOf('.') + 1);
+            String methodName = description.getMethodName();
+            takeScreenshot(className + "#" + methodName);
+        }
+    };
+
+    @Rule
+    public GrantPermissionRule mRuntimePermissionRule = GrantPermissionRule
+            .grant(
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                    Manifest.permission.READ_EXTERNAL_STORAGE
+            );
+
+    @Rule
     public ActivityTestRule<MainActivity> activityActivityTestRule = new ActivityTestRule<>(MainActivity.class);
 
 
     @Test
+    @DisplayName("Check intent")
     public void checkIntent() {
         String url = "https://google.com";
         ViewInteraction options = onView(
@@ -57,6 +90,7 @@ public class TestsHomework2 {
     }
 
     @Test
+    @DisplayName("Check List")
     public void checkList() {
         ViewInteraction navigation = onView(
                 withContentDescription("Open navigation drawer")
@@ -79,5 +113,22 @@ public class TestsHomework2 {
     @After
     public void unregisterIdlingResources() {
         IdlingRegistry.getInstance().unregister(IdlingResources.idlingResource);
+    }
+
+    private void takeScreenshot(String name) {
+        File path = new File(Environment.getExternalStorageDirectory()
+                .getAbsolutePath() + "/screenshots/" );
+        if (!path.exists()) {
+            path.mkdirs();
+        }
+        UiDevice device = UiDevice.getInstance(getInstrumentation());
+        String filename = name + ".png";
+        device.takeScreenshot(new File(path, filename));
+        try {
+            Allure.attachment(filename, new FileInputStream(new File(path, filename)));
+        }
+        catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
     }
 }
